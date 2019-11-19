1ffde506feae76d2fa813656a1604c40b7b1b677
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2019-11-19 20:25:09 +0300

    controller and service updated for examination

--- petMedDatabaseWebapp/src/main/java/com/petmed/petMedicineDatabaseWebapp/service/ExaminationService.java
+++ petMedDatabaseWebapp/src/main/java/com/petmed/petMedicineDatabaseWebapp/service/ExaminationService.java
@@ -1,8 +1,56 @@
 package com.petmed.petMedicineDatabaseWebapp.service;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.petmed.petMedicineDatabaseWebapp.entity.Vet;
+import com.petmed.petMedicineDatabaseWebapp.entity.Animal;
+import com.petmed.petMedicineDatabaseWebapp.entity.Owner;
+import com.petmed.petMedicineDatabaseWebapp.entity.Examination;
 
 @Service
 public class ExaminationService {
+	
+	public List<Examination> getExaminatios(){
+		return null;
+	}
+	
+	public Examination getExamination(int appointmentId) {
+		return null;
+	}
+
+	public void saveExamination(Examination newExamination) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public LinkedHashMap<String, Animal> getAnimals() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Vet getVets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public LinkedHashMap<Animal, String> getAnimalsAsKeyValuePair() {
+		//icini doldur bana gonder abi
+		LinkedHashMap<Animal,String> animalList = new LinkedHashMap<Animal, String>();
+		return animalList;
+	}
+
+	public LinkedHashMap<Vet, String> getVetsAsKeyValuePair() {
+		// TODO Auto-generated method stub
+		//icini doldur bana gonder abi
+		LinkedHashMap<Vet,String> vetList = new LinkedHashMap<Vet, String>();
+		return vetList;
+	}
+	 
+
+	
 
 }
--- petMedDatabaseWebapp/src/main/webapp/WEB-INF/view/list-examination.jsp
+++ petMedDatabaseWebapp/src/main/webapp/WEB-INF/view/list-examination.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +10,25 @@
 <body>
 <h1> Examination list </h1>
 This page is under construction.
+
+<table>
+	<tr>
+ 	<th>Tarih</th>
+ 	<th>Evcil Hayvan</th>
+ 	<th>Sorumlu Veteriner</th>
+ 	<th>Sahibi</th>
+	</tr>
+	
+	<c:forEach var="tempExamination" items="${examinations}">
+	<tr>
+				<td> ${tempExamination.appointmentDate} </td>
+				<td> ${tempExamination.animal.name} </td>
+				<td> ${tempExamination.vet.name} </td>
+				<td> ${tempExamination.owner.name}</td>
+	</tr>
+	</c:forEach>
+</table>
+
 
 </body>
 </html>
