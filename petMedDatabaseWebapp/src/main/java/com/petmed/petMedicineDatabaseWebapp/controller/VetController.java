ab557aac47f274f8812de3d61dbdc54e35a78ca0
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2019-11-20 23:56:02 +0300

    VetController.java is filled

--- petMedDatabaseWebapp/src/main/java/com/petmed/petMedicineDatabaseWebapp/controller/VetController.java
+++ petMedDatabaseWebapp/src/main/java/com/petmed/petMedicineDatabaseWebapp/controller/VetController.java
@@ -7,10 +7,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vet")
 public class VetController {
 	
+	@Autowired
+	private VetService vetService; 
+	
 	@RequestMapping("/list")
-	public String listAnimals() {
+	public String listVets(Model theModel) {
+		List<Vet> vetList = vetService.getVets();
+		
+		theModel.addAttribute(vetList);
 		
 		return "list-vet";
 	}
-
+	
+	@GetMapping("/showVetFormForAdd")
+	public String showVetFormForAdd(Model theModel) {
+		Vet theVet = new Vet();
+		
+		theModel.addAttribute("vet",theVet);
+		
+		return "vetForm";
+		
+	}
+	
+	@PostMapping("/saveVet")
+	public String saveVet(@ModelAttribute("vet") Vet newVet) {
+		vetService.saveVet(newVet);
+		
+		return "redirect:/vet/list";
+	}
+	
+	@GetMapping("/showVetFormForUpdate")
+	public String showVetFormForUpdate(@RequestParam("vetTCNo") int theId, Model theModel) {
+		
+		Vet theVet = vetService.getVet(theId);
+		
+		theModel.addAttribute(theVet);
+		
+		return "vet-form";
+	}
 }
